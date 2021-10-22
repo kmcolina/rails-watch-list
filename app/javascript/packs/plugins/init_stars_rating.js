b1e1411da45f81d53cbdf9e47bc243d9930ce95c
katherine colina <kathymcolina@gmail.com>
2021-10-22 18:04:25 -0400

    add stars review

--- app/javascript/packs/plugins/init_stars_rating.js
+++ app/javascript/packs/plugins/init_stars_rating.js
@@ -1,9 +1,25 @@
 import 'jquery-bar-rating';
 
+// const initStarsRating = () => {
+//   $('#example').barrating({
+//     theme: 'css-stars'
+//   });
+// };
+
+// const initStarsRating = () => {
+//   $('#review_rating').barrating({
+//     theme: 'css-stars',
+//     onSelect: (value, text, event) => {
+//       const form = $("form.review_form"); // We are selecting the form on the page with its class
+//       form.submit(); // We submit the form with javascript
+//     }
+//   });
+// };
+
+
 const initStarsRating = () => {
-  $('#example').barrating({
+  $('#review_rating').barrating({
     theme: 'css-stars'
   });
 };
-
 export { initStarsRating };
